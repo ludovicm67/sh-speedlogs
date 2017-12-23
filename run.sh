af001128297fb1a6d83bcf4a4614544e6a59a364
ludovicm67 <ludovicmuller1@gmail.com>
2017-12-23 18:07:43 +0100

    updated formatting

--- run.sh
+++ run.sh
@@ -2,4 +2,22 @@
 
 speedtest-cli --json \
   | jq --compact-output \
-    '{ping: .ping, down: .download, up: .upload, time: .timestamp} | to_entries | map(if .key == "down" or .key == "up" then .value/=1000000 else . end) | from_entries'
+    '{
+      ping: .ping,
+      down: .download,
+      up: .upload,
+      time: .timestamp
+    }
+    | to_entries
+    | map(
+      if
+        .key == "down"
+        or
+        .key == "up"
+      then
+        .value/=1000000
+      else
+        .
+      end
+    )
+    | from_entries'
