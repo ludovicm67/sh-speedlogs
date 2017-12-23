47cce1ecf2768b5613e637fdd470694c7893e242
ludovicm67 <ludovicmuller1@gmail.com>
2017-12-23 18:28:46 +0100

    error handling

--- run.sh
+++ run.sh
@@ -1,23 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-speedtest-cli --json \
-  | jq --compact-output \
-    '{
-      ping: .ping,
-      down: .download,
-      up: .upload,
-      time: .timestamp
-    }
-    | to_entries
-    | map(
-      if
-        .key == "down"
-        or
-        .key == "up"
-      then
-        .value/=1000000
-      else
-        .
-      end
-    )
-    | from_entries'
+SPEEDTEST_RESULTS=`speedtest-cli --json 2> /dev/null`
+
+[[ $? == 0 ]] \
+  && echo "$SPEEDTEST_RESULTS" \
+    | jq --compact-output \
+      '{
+        ping: .ping,
+        down: .download,
+        up: .upload,
+        time: .timestamp
+      }
+      | to_entries
+      | map(
+        if
+          .key == "down"
+          or
+          .key == "up"
+        then
+          .value/=1000000
+        else
+          .
+        end
+      )
+      | from_entries' \
+  || echo "Connection error."
