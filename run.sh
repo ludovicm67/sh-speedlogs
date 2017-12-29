a830400295c0959bd462b35fe47c0cac9ec29928
ludovicm67 <ludovicmuller1@gmail.com>
2017-12-29 17:50:51 +0100

    close #2 : clean outputs

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# some variables
+CURRENT_DIRECTORY=$(cd `dirname $0` && pwd)
+
 # check existance of a command passed in arg $1
 check_existence() {
   command -v "$1" > /dev/null 2>&1 \
     || ( \
-      echo "Missing command '$1'."; \
-      echo "Try installing it using 'sudo apt install $1'."; \
+      echo "Missing command '$1'." 1>&2; \
+      echo "Try installing it using 'sudo apt install $1'." 1>&2; \
       exit 1 \
     )
 }
@@ -20,7 +23,7 @@ SPEEDTEST_RESULTS=`speedtest-cli --json 2> /dev/null`
 # generate a simple JSON
 [[ $? == 0 ]] \
   && echo "$SPEEDTEST_RESULTS" \
-    | jq --compact-output \
+    | jq -r --compact-output \
       '{
         ping: .ping,
         down: .download,
@@ -40,4 +43,16 @@ SPEEDTEST_RESULTS=`speedtest-cli --json 2> /dev/null`
         end
       )
       | from_entries' \
-  || echo "Connection error."
+  || ( \
+      echo "Connection error." 1>&2; \
+      CURRENT_DATE_UTC=`date -u +%FT%T.%6NZ`
+      echo '{
+        "ping":-1,
+        "down":0,
+        "up":0,
+        "time":"'"$CURRENT_DATE_UTC"'"
+      }' | jq -r --compact-output .; \
+      exit 1;
+    )
+
+exit 0
